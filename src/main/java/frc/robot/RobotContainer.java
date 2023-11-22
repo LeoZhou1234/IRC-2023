7f5a18031f73a05b26b937696d8056fafa08d253
unknown <leoyihanzhou@gmail.com>
2023-11-22 14:56:42 -0800

    auto code ver1 + fixes(?)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,20 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.AutoDrive;
 import frc.robot.commands.Drive;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.SetConveyorSpeed;
+import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.Joystick;
+
+import java.util.HashSet;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -22,14 +29,18 @@ import edu.wpi.first.wpilibj.Joystick;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+//  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+//
+//  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   Joystick leftJoystick = new Joystick(0);
-  Joystick rightJoystick = new Joystick(1);
+//  Joystick rightJoystick = new Joystick(1);
   Drivetrain drivetrain = new Drivetrain();
+  Conveyor conveyor = new Conveyor(new VictorSP(2));
   Drive drive = new Drive(drivetrain, leftJoystick);
+
+  JoystickButton joystickButton = new JoystickButton(leftJoystick, 3);
+  JoystickButton joystickButton2 = new JoystickButton(leftJoystick, 2);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -44,7 +55,32 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    joystickButton
+            .whileTrue(new SetConveyorSpeed(conveyor, 1D))
+            .whileFalse(() -> {
+              if (joystickButton2.getAsBoolean()) {
+                new SetConveyorSpeed(conveyor, -1D).execute();
+              } else {
+                new SetConveyorSpeed(conveyor, 0D).execute();
+              }
+                HashSet<Subsystem> ret = new HashSet<>();
+                ret.add(conveyor);
+                return ret;
+            });
+    joystickButton2
+            .whileTrue(new SetConveyorSpeed(conveyor, -1D))
+            .whileFalse(() -> {
+              if (joystickButton.getAsBoolean()) {
+                new SetConveyorSpeed(conveyor, 1D).execute();
+              } else {
+                new SetConveyorSpeed(conveyor, 0D).execute();
+              }
+              HashSet<Subsystem> ret = new HashSet<>();
+              ret.add(conveyor);
+              return ret;
+            });
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
