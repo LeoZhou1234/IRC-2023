ec666ab072b3ee20d95704528a064bbde13080f6
q13x <84165981+WorldEditAxe@users.noreply.github.com>
2023-11-30 01:53:06 -0800

    read encoded commands from deploy/commands.txt

--- src/main/java/frc/robot/commands/AutoPlaybackCommand.java
+++ src/main/java/frc/robot/commands/AutoPlaybackCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.recorder.PlaybackService;
 import frc.robot.recorder.RecordingFrameArray;
@@ -7,9 +8,22 @@ import frc.robot.recorder.Unrecordable;
 import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.Drivetrain;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @Unrecordable
 public class AutoPlaybackCommand extends CommandBase {
-    public static final String ENCODED_COMMANDS = "G7+AAAAAAAAAP5AgQIAAAAC5AgEAAAAGP7IkSKAAAAC/tgAAAAAAACgAP7o0aOAAAAC/xwAAAAAAACgAP7o0aOAAAAC/0YAAAAAAACgAAAAAAAAAAAC/4IAAAAAAACgAv5gAAAAAAAC/54AAAAAAACgAv6QAAAAAAAC/7YAAAAAAACgAv6QAAAAAAAC/8AAAAAAAAFAAP5AgQIAAAAC/8AAAAAAAACgAP7AgQIAAAAC/8AAAAAAAACgAP7YsWMAAAAC/6gAAAAAAACgAP7o0aOAAAAC/5wAAAAAAACgAP748eQAAAAC/34AAAAAAACgAP6AgQIAAAAC/tgAAAAAAACgAAAAAAAAAAAA/oCBAgAAAACgAv4AAAAAAAAA/xChQoAAAACgAP7IkSKAAAAA/2LFiwAAAACgAP848eQAAAAA/46dOoAAAACgAP9ChQoAAAAA/6PHjwAAAACgAP9GjRoAAAAA/6rVq4AAAACgAP8o0aOAAAAA/6rVq4AAAAFAAP9KlSqAAAAA/67du4AAAACgAP9SpUqAAAAA/7z59AAAAAFAAP9YsWMAAAAA/7Xr14AAAACgAP8o0aOAAAAA/6LFiwAAAACgAP5AgQIAAAAA/2rVq4AAAACgAv4AAAAAAAAC/gAAAAAAAAIgEAA==";
+    public static final String ENCODED_COMMANDS;
+
+    static {
+        try {
+            ENCODED_COMMANDS = new String(Files.readAllBytes(Paths.get(Filesystem.getDeployDirectory().getAbsolutePath(), "commands.txt")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     Drivetrain dt;
     Conveyor cv;
     String encoded = null;
